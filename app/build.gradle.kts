a36858f2af146aac37e502be639d68af068c6461
polaralias <76123410+polaralias@users.noreply.github.com>
2026-01-18 19:51:42 +0000

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,14 +49,13 @@ dependencies {
     val roomVersion = "2.6.1"
 
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
-    implementation(platform("androidx.lifecycle:lifecycle-bom:2.7.0"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
